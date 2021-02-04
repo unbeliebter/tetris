3386fa476637ab9f5e53de174ba6b006d436989b
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-02-04 21:56:56 +0100

    fixed maybe a major bug

--- src/Tetris.java
+++ src/Tetris.java
@@ -2,5 +2,6 @@ public class Tetris {
     public static void main(String[] args) {
         Tetromino test = new Tetromino(1);
         System.out.println(test.x1);
+        System.out.println("FIX");
     }
 }
