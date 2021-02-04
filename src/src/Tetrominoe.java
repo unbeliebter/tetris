07cc49caf1ad1e6019b88f854cf3f7ce69659055
Fawie <78504668+Fawie@users.noreply.github.com>
2021-02-04 15:17:55 +0100

    add class Tetrominoe, define different Tetrominoes

---
+++ src/src/Tetrominoe.java
@@ -0,0 +1,26 @@
+public Tetrominoe{
+
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        int x3;
+        int y3;
+        int x4;
+        int y4;
+
+public void Tetrominoe(int n){
+        switch(n){
+        case(1): //L-Shape
+        x1 = 0;
+        y1 = 38;
+        x2 = 0;
+        y2 = 39;
+        x3 = 1;
+        y3 =
+        break;
+
+        }
+        }
+
+        }
