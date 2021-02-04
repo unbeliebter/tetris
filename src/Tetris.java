58c6dfb5e5dd002cdb352d17e50b0c17e966b35d
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-02-04 23:37:19 +0100

    add a frame

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,7 +1,15 @@
+import javax.swing.*;
+
 public class Tetris {
     public static void main(String[] args) {
         Tetromino test = new Tetromino(1);
-        System.out.println(test.x1);
-        System.out.println("FIX");
+
+        JFrame frame = new JFrame("Tetris");
+        frame.setSize(600, 800);
+        frame.setTitle("Tetris");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        frame.setVisible(true);
     }
 }
