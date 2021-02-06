a4a88cc43da78f453bc0df0c916bfaa0993a2cc3
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-02-06 01:10:51 +0100

    defined a PlayField

--- src/playfield/PlayField.java
+++ src/playfield/PlayField.java
@@ -1,4 +1,18 @@
 package playfield;
 
+import tetrominos.Tetromino;
+
 public class PlayField extends PlayFieldCanvas {
+
+    public static int xMax;
+    public static int yMax;
+    public int[][] array;
+    public int points;
+    public Tetromino liveTetromino;
+
+    public PlayField() {
+        xMax = 10;
+        yMax = 40;
+        array = new int[xMax + 2][yMax + 2];
+    }
 }
