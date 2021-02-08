eeaf40080b5c9f12b0e466fe861f96e41e990fb7
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-02-08 20:30:16 +0100

    small fix

--- src/playfield/PlayFieldCanvas.java
+++ src/playfield/PlayFieldCanvas.java
@@ -7,7 +7,7 @@ import java.text.AttributedCharacterIterator;
 
 public class PlayFieldCanvas extends JComponent {
 
-    private static int wide = PlayField.getyMax();
+    private static int wide = PlayField.yMax;
     private static PlayField playField;
 
     public PlayFieldCanvas(PlayField actualPlayField) {
