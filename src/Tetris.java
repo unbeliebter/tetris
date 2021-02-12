90a04291e513bd11e4b634d405b6d8de0154bc75
Fawie <78504668+Fawie@users.noreply.github.com>
2021-02-12 22:15:22 +0100

    solved conflicts

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,37 +1,27 @@
 import playfield.PlayField;
-import playfield.PlayFieldCanvas;
+import playfield.PlayFieldJframe;
 import tetrominos.Tetromino;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Tetris {
 
     public Tetris() {
 
     }
-
     public static void main(String[] args) {
         Tetromino test = new Tetromino(0);
         Tetris tetris = new Tetris();
         test.rotate();
-        System.out.println(test.getX1() + "/" + test.getY1());
-        System.out.println(test.getX2() + "/" + test.getY2());
-        System.out.println(test.getX3() + "/" + test.getY3());
-        System.out.println(test.getX4() + "/" + test.getY4());
+        System.out.println(test.getX1()+"/"+test.getY1());
+        System.out.println(test.getX2()+"/"+test.getY2());
+        System.out.println(test.getX3()+"/"+test.getY3());
+        System.out.println(test.getX4()+"/"+test.getY4());
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        PlayField playField = new PlayField();
-        System.out.println("TEST");
-        frame.add(new PlayFieldCanvas(playField));
-        frame.setSize(900, 800);
-        System.out.println("TEST2");
-        frame.setVisible(true);
-        playField.newTetromino(1);
-        playField.deleterow(1);
-
-
+        SwingUtilities.invokeLater(() -> {
+            PlayFieldJframe.createPlayFieldJFrame();
+        });
     }
+
+
 }
