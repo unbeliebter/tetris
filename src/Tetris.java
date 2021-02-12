b6e8c7fac7695222fd37351adfa0e657d73357f9
Sven <42941230+unbeliebter@users.noreply.github.com>
2021-02-12 22:02:13 +0100

    deleted things of Canvas

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,9 +1,7 @@
 import playfield.PlayField;
-import playfield.PlayFieldCanvas;
 import tetrominos.Tetromino;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Tetris {
 
@@ -18,17 +16,6 @@ public class Tetris {
         System.out.println(test.getX2()+"/"+test.getY2());
         System.out.println(test.getX3()+"/"+test.getY3());
         System.out.println(test.getX4()+"/"+test.getY4());
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        PlayField playField = new PlayField();
-        System.out.println("TEST");
-        frame.add(new PlayFieldCanvas(playField));
-        frame.setSize(900, 800);
-        System.out.println("TEST2");
-        frame.setVisible(true);
-
     }
 
 
