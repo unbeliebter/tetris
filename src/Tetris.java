5677de165889496409e62f02188d7b0bdea1bd49
Fawie <78504668+Fawie@users.noreply.github.com>
2021-02-12 22:06:39 +0100

    added deleterow method and added newTetromino Method

--- src/Tetris.java
+++ src/Tetris.java
@@ -10,14 +10,15 @@ public class Tetris {
     public Tetris() {
 
     }
+
     public static void main(String[] args) {
         Tetromino test = new Tetromino(0);
         Tetris tetris = new Tetris();
         test.rotate();
-        System.out.println(test.getX1()+"/"+test.getY1());
-        System.out.println(test.getX2()+"/"+test.getY2());
-        System.out.println(test.getX3()+"/"+test.getY3());
-        System.out.println(test.getX4()+"/"+test.getY4());
+        System.out.println(test.getX1() + "/" + test.getY1());
+        System.out.println(test.getX2() + "/" + test.getY2());
+        System.out.println(test.getX3() + "/" + test.getY3());
+        System.out.println(test.getX4() + "/" + test.getY4());
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,8 +29,9 @@ public class Tetris {
         frame.setSize(900, 800);
         System.out.println("TEST2");
         frame.setVisible(true);
+        playField.newTetromino(1);
+        playField.deleterow(1);
+
 
     }
-
-
 }
